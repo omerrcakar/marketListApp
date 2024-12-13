7a2686e26d2dd101d0918130e8871af038bc1e40
omer cakar <omerrccak@gmail.com>
2024-12-13 15:33:24 +0300

    market List app created

---
+++ marketList/marketList/marketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketListApp.swift
+//  marketList
+//
+//  Created by ÖMER  on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct marketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
